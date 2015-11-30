234633bfa5f897e63b2ce45b674089a71be43e79
Gustavo Lima <gustavo.lima@fabamaq.com>
2015-11-30 11:23:26 +0000

    Added sorting to files list

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,70 @@
 var LOCALSTORAGE_NAME = "files-store";
 
-var filer = {},
+var Filer = {},
     initialData = [
         {"filename": "index.html", "size": 13},
         {"filename": "contact.html", "size": 12},
         {"filename": "sample.png", "size": 65}
     ];
 
-filer.File = Backbone.Model.extend({
+Filer.File = Backbone.Model.extend({
+    defaults: {
+        "filename": undefined
+    },
     initialize : function() {
         // Set fileType attribute according to file extension
-        var fileType, extension = this.attributes.filename.split(".").pop();
+        var filetype, extension = this.attributes.filename.split(".").pop();
         switch( extension ) {
             case "html":
-                fileType = "Web page";
+                filetype = "Web page";
                 break;
             case "png":
-                fileType = "PNG image";
+                filetype = "PNG image";
                 break;
             case "jpeg":
             case "jpg":
-                fileType = "JPEG image";
+                filetype = "JPEG image";
                 break;
             case "txt":
-                fileType = "Text file";
+                filetype = "Text file";
                 break;
             default:
-                fileType = "Unknown file type"
+                filetype = "Unknown file type"
         }
 
-        this.set('fileType', fileType);
+        this.set('filetype', filetype);
     }
 });
 
-filer.Files = Backbone.Collection.extend({
-    model: filer.File,
+Filer.FileCollection = Backbone.Collection.extend({
+    model: Filer.File,
     localStorage: new Backbone.LocalStorage(LOCALSTORAGE_NAME),
+
+    // Available sort strategies
+    strategies: {
+        filename: function (file) { return file.get("filename"); },
+        filetype: function (file) { return file.get("filetype"); },
+        size: function (file) { return file.get("size"); },
+    },
+
+    // Current property defined as sort attribute
+    sortAttribute: null,
+
+    // Sort order
+    sortOrder: 1,
+
+    // Change sort property
+    changeSort: function (sortProperty) {
+        this.comparator = this.strategies[sortProperty];
+
+        if (this.sortOrder < 0) {
+            this.comparator = reverseSortBy(this.comparator);
+        }
+    },
+
+    changeSortOrder: function() {
+        this.sortOrder = -1 * this.sortOrder;
+    },
 
     // Override fetch method to check if localStorage is set
     // If not set load data from JSON
@@ -50,14 +79,28 @@ filer.Files = Backbone.Collection.extend({
     }
 });
 
-// Load initial data into files collection
-var filesCollection = new filer.Files();
+function reverseSortBy(sortByFunction) {
+    return function(left, right) {
+        var l = sortByFunction(left);
+        var r = sortByFunction(right);
 
+        if (l === void 0) return -1;
+        if (r === void 0) return 1;
+
+        return l < r ? 1 : l > r ? -1 : 0;
+    };
+}
 
 var FileListView = Backbone.View.extend({
     el: '#container',
-    tagName: 'ul',
+    tagName: 'table',
     className: 'file-list',
+
+    // Need to respond to clicks on the table headers
+    events: {
+        "click th.sortable": "headerClick"
+    },
+
     initialize: function() {
         this.template = _.template($('#file-list-template').html());
         this.render();
@@ -70,17 +113,51 @@ var FileListView = Backbone.View.extend({
     render: function() {
         this.$el.empty();
         this.$el.append(this.template());
+
         return this;
     },
     renderFile: function(files) {
         var newFileView = new FileView({model: files.toJSON()});
         files.save();
 
-        this.$el.append(newFileView.render().el);
+        var $tbody = this.$("tbody");
+        $tbody.append(newFileView.render().el);
+    },
+    headerClick: function(event){
+        var $el = $(event.currentTarget),
+            sortProperty = $el.attr("data-sort");
+
+        // If column is already sorting then change sorting order
+        // by calling collection's method and change element's classes
+        if ($el.hasClass("selected")) {
+            if ($el.hasClass("asc")) {
+                $el
+                    .addClass("desc")
+                    .removeClass("asc");
+            } else {
+                $el
+                    .addClass("asc")
+                    .removeClass("desc");
+            }
+
+            this.collection.changeSortOrder();
+        }
+
+        // Set sort property and call sort
+        this.collection.changeSort(sortProperty);
+        this.collection.sort();
+
+        // Rerender file list
+        this.$("tbody").empty();
+        this.collection.each(this.renderFile);
+
+        $el.siblings(".selected").removeClass("selected");
+        $el.addClass("selected");
     }
 });
 
 var FileView = Backbone.View.extend({
+    tagName: 'tr',
     className: 'file',
     initialize: function() {
         this.template = _.template($('#file-template').html());
@@ -92,6 +169,6 @@ var FileView = Backbone.View.extend({
     }
 });
 
+// Load initial data into files collection
+var filesCollection = new Filer.FileCollection();
 var filerAppView = new FileListView({collection: filesCollection});
-
-//filesCollection.add(initialData);
